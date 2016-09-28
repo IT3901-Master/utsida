85282d4b5aeb5909f16bf2dd504f3f1c35f21812
Truls Mørk Pettersen <trulsmp@stud.ntnu.no>
2016-09-28 16:37:23 +0200

    django scripting

---
+++ testScript.py
@@ -0,0 +1,24 @@
+# template script file to use for future reference
+
+
+import os, sys
+
+proj_path = "/Users/trulsmp/Documents/master/utsida"
+# This is so Django knows where to find stuff.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "utsida.settings")
+sys.path.append(proj_path)
+
+# This is so my local_settings.py gets loaded.
+os.chdir(proj_path)
+
+# This is so models get loaded.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+from utsida.models import *
+
+faculty = Faculty(name="Fakultet for informasjonsteknologi, matematikk og elektroteknikk", acronym="IME")
+institute = Institute(name="Institutt for datateknikk og informasjonsvitenskap",acronym="IDI",faculty=faculty)
+
+faculty.save()
+institute.save()
