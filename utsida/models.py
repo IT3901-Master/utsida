601035fc83e82c713616a4dfefc0db2a28e5695b
Truls Mørk Pettersen <trulsmp@stud.ntnu.no>
2016-09-28 15:34:38 +0200

    added string representation to models

--- utsida/models.py
+++ utsida/models.py
@@ -6,21 +6,34 @@ CHOICES = ((1,1),(2,2),(3,3),(4,4),(5,5))
 class Faculty(models.Model):
     name = models.CharField(max_length=30, primary_key=True)
 
+    def __str__(self):
+        return self.name
+
 class Institute(models.Model):
     name = models.CharField(max_length=30, primary_key=True)
     faculty = models.ForeignKey(Faculty)
 
+    def __str__(self):
+        return self.name
 
 class Country(models.Model):
     name = models.CharField(max_length=30, primary_key=True)
+
+    def __str__(self):
+        return self.name
 
 
 class University(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
 
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     name = models.CharField(max_length=10, primary_key=True)
 
+    def __str__(self):
+        return self.name
 
 
 
@@ -35,3 +48,6 @@ class Case(models.Model):
     socialQualityRating = models.IntegerField(choices=CHOICES)
     careerAdvantagesRating = models.IntegerField(choices=CHOICES)
     subjects = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.university.name + ':' + self.pk
