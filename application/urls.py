0f65f22f1cf972f188d1d6a4ee4f8045ecf8eb53
Truls Mørk Pettersen <trulsmp@stud.ntnu.no>
2016-10-05 16:48:32 +0200

    push stuff

--- application/urls.py
+++ application/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^profile/', include("profiles.urls"), name='profiles'),
     url('^', include("utsida.urls"), name="index"),
+<<<<<<< HEAD
+=======
+
+>>>>>>> e33d957817159d596fc80a4193f71c66b338a6f0
 ]
