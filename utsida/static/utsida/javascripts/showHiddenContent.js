09f00a03589b6398af6d434a5bd83efa57811213
Lars Liverød Andersen <lars_la@live.com>
2016-10-20 17:05:31 +0200

    + CSS imporvements for trip representation

--- utsida/static/utsida/javascripts/showHiddenContent.js
+++ utsida/static/utsida/javascripts/showHiddenContent.js
@@ -3,12 +3,13 @@ var acc = document.getElementsByClassName("accordion");
 for (var i = 0; i < acc.length; i++) {
     acc[i].onclick = function() {
         this.classList.toggle("active");
-        this.previousElementSibling.classList.toggle("show");
-        if (this.firstChild.innerHTML === "Vis ekstra informasjon") {
-            this.firstChild.innerHTML = "Skjul ekstra informasjon";
+        this.parentNode.previousElementSibling.classList.toggle("show");
+        if (this.classList.contains("glyphicon-chevron-down")) {
+            this.className = "accordion glyphicon glyphicon-chevron-up";
         }
-        else if (this.firstChild.innerHTML === "Skjul ekstra informasjon"){
-            this.firstChild.innerHTML = "Vis ekstra informasjon";
+        else if (this.classList.contains("glyphicon-chevron-up")){
+            this.className = "accordion glyphicon glyphicon-chevron-down";
         }
     }
 }
+
