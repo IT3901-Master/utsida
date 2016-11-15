08390795f15796d88a53c2410ee82bd72007764f
Lars Liverød Andersen <lars_la@live.com>
2016-11-15 18:22:37 +0100

    * Some text- and css changed. * Added a new panel in the results page, which displays which courses the user has added.
    * Made it possible to click on courses to add them to their list.
    * Added tools for using bootstrap popovers.
    * Stuff.

---
+++ utsida/static/utsida/javascripts/result/courseSelector.js
@@ -0,0 +1,42 @@
+var selectedCourses = [];
+var numSelectedCourses = 0;
+var selectedCourseBody = document.getElementById("selectedCourseBody");
+var selectedCourseToggleBtn = document.getElementById("selectedCourseToggleBtn");
+var numSelectedCoursesBadge = document.getElementById("numSelectedCoursesBadge");
+var selectedCourseFooter = document.getElementById("selectedCourseFooter");
+var selectedCourseList = document.getElementById("selectedCourseList");
+var selectedCourseContainer = document.getElementById("selectedCourseContainer");
+
+function addCourse(course) {
+    selectedCourseContainer.style.display = "block";
+    if (!(selectedCourses.indexOf(course) > -1)) {
+        selectedCourses.push(course);
+        var label = document.createElement("li");
+        label.innerHTML = course;
+        selectedCourseList.appendChild(label);
+        numSelectedCourses += 1;
+        updateNumSelectedCourses();
+    }
+}
+
+function toggleSelectedCourses() {
+    selectedCourseBody.style.display = selectedCourseBody.style.display == "none" ? "block" : "none";
+    selectedCourseToggleBtn.className = selectedCourseToggleBtn.className == "pull-right glyphicon glyphicon-chevron-down" ? "pull-right glyphicon glyphicon-chevron-up" : "pull-right glyphicon glyphicon-chevron-down";
+    selectedCourseFooter.style.display = selectedCourseFooter.style.display == "none" ? "block" : "none";
+}
+
+function removeAllSelectedCourses() {
+    selectedCourses = [];
+    numSelectedCourses = 0;
+    updateNumSelectedCourses();
+    while(selectedCourseBody.firstChild) {
+        selectedCourseBody.removeChild(selectedCourseBody.firstChild)
+    }
+    toggleSelectedCourses();
+
+}
+
+function updateNumSelectedCourses() {
+    numSelectedCoursesBadge.innerHTML = numSelectedCourses.toString();
+
+}
