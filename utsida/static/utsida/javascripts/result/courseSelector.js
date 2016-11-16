9c413d49985247704e1d9b8bac69ff48327320de
Lars Liverød Andersen <lars_la@live.com>
2016-11-16 14:02:49 +0100

    Rewrote the course selector script to model-pattern

--- utsida/static/utsida/javascripts/result/courseSelector.js
+++ utsida/static/utsida/javascripts/result/courseSelector.js
@@ -1,48 +1,61 @@
-var selectedCourses = [];
-var numSelectedCourses = 0;
-var selectedCourseBody = document.getElementById("selectedCourseBody");
-var selectedCourseToggleBtn = document.getElementById("selectedCourseToggleBtn");
-var numSelectedCoursesBadge = document.getElementById("numSelectedCoursesBadge");
-var selectedCourseFooter = document.getElementById("selectedCourseFooter");
-var selectedCourseList = document.getElementById("selectedCourseList");
-var selectedCourseContainer = document.getElementById("selectedCourseContainer");
+var s;
+courseSelector = {
 
-function addCourse(course) {
-    showContainer();
-    if (!(selectedCourses.indexOf(course) > -1)) {
-        selectedCourses.push(course);
-        var label = document.createElement("li");
-        label.innerHTML = course;
-        selectedCourseList.appendChild(label);
-        numSelectedCourses += 1;
-        updateNumSelectedCourses();
+    settings: {
+        selectedCourses: [],
+        numSelectedCourses: 0,
+        selectedCourseBody: document.getElementById("selectedCourseBody"),
+        selectedCourseToggleBtn: document.getElementById("selectedCourseToggleBtn"),
+        numSelectedCoursesBadge: document.getElementById("numSelectedCoursesBadge"),
+        selectedCourseFooter: document.getElementById("selectedCourseFooter"),
+        selectedCourseList: document.getElementById("selectedCourseList"),
+        selectedCourseContainer: document.getElementById("selectedCourseContainer")
+    },
+
+    init: function() {
+        s = this.settings;
+    },
+
+    addCourse: function(course) {
+        this.showContainer();
+        if (!(s.selectedCourses.indexOf(course) > -1)) {
+            s.selectedCourses.push(course);
+            var label = document.createElement("li");
+            label.innerHTML = course;
+            s.selectedCourseList.appendChild(label);
+            s.numSelectedCourses += 1;
+            this.updateNumSelectedCourses();
+        }
+    },
+
+    removeAllSelectedCourses: function() {
+        s.selectedCourses = [];
+        s.numSelectedCourses = 0;
+        this.updateNumSelectedCourses();
+        while(s.selectedCourseList.firstChild) {
+            s.selectedCourseList.removeChild(s.selectedCourseList.firstChild)
+        }
+        this.toggleSelectedCourses();
+        this.hideContainer();
+    },
+
+    updateNumSelectedCourses: function() {
+        s.numSelectedCoursesBadge.innerHTML = s.numSelectedCourses.toString();
+    },
+
+    toggleSelectedCourses: function() {
+        s.selectedCourseBody.style.display = s.selectedCourseBody.style.display == "none" ? "block" : "none";
+        s.selectedCourseToggleBtn.className = s.selectedCourseToggleBtn.className == "pull-right glyphicon glyphicon-chevron-down" ? "pull-right glyphicon glyphicon-chevron-up" : "pull-right glyphicon glyphicon-chevron-down";
+        s.selectedCourseFooter.style.display = s.selectedCourseFooter.style.display == "none" ? "block" : "none";
+    },
+
+    hideContainer: function() {
+        s.selectedCourseContainer.style.display = "none";
+    },
+
+    showContainer: function() {
+        s.selectedCourseContainer.style.display = "block";
     }
-}
+};
 
-function toggleSelectedCourses() {
-    selectedCourseBody.style.display = selectedCourseBody.style.display == "none" ? "block" : "none";
-    selectedCourseToggleBtn.className = selectedCourseToggleBtn.className == "pull-right glyphicon glyphicon-chevron-down" ? "pull-right glyphicon glyphicon-chevron-up" : "pull-right glyphicon glyphicon-chevron-down";
-    selectedCourseFooter.style.display = selectedCourseFooter.style.display == "none" ? "block" : "none";
-}
-
-function removeAllSelectedCourses() {
-    selectedCourses = [];
-    numSelectedCourses = 0;
-    updateNumSelectedCourses();
-    while(selectedCourseList.firstChild) {
-        selectedCourseList.removeChild(selectedCourseList.firstChild)
-    }
-    toggleSelectedCourses();
-    hideContainer();
-}
-
-function updateNumSelectedCourses() {
-    numSelectedCoursesBadge.innerHTML = numSelectedCourses.toString();
-}
-
-function hideContainer() {
-    selectedCourseContainer.style.display = "none";
-}
-function showContainer() {
-    selectedCourseContainer.style.display = "block";
-}
+courseSelector.init();
