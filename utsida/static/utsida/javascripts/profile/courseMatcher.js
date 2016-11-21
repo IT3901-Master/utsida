bacd1e3461eb35632b362e91387632153b329742
Lars Liverød Andersen <lars_la@live.com>
2016-11-21 15:00:13 +0100

    stuff

---
+++ utsida/static/utsida/javascripts/profile/courseMatcher.js
@@ -0,0 +1,19 @@
+CourseMatcher = {
+
+    s: {
+        awayCourses: null,
+        homeCourses: null,
+    },
+
+    init: function() {
+        s.awayCourses = document.getElementById("courseList");
+        s.homeCourses = document.getElementById("homeCourseList");
+    },
+
+    matchSelectedCourses: function() {
+
+
+    }
+};
+
+CourseMatcher.init();
