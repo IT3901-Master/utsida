21211daf25cc4d7ce2eb8e81962f201dc134d156
Lars Liverød Andersen <lars_la@live.com>
2016-12-06 16:44:40 +0100

    Reducing long text, hoverable courses

--- utsida/static/utsida/javascripts/result/showHiddenContent.js
+++ utsida/static/utsida/javascripts/result/showHiddenContent.js
@@ -1,6 +1,7 @@
 // Logic for accordion functionality on the case results in /results.
 
 var acc = document.getElementsByClassName("accordion");
+var headers = document.getElementsByClassName("panel-heading square-corners");
 
 for (var i = 0; i < acc.length; i++) {
     acc[i].onclick = function() {
@@ -14,4 +15,25 @@ for (var i = 0; i < acc.length; i++) {
         }
     }
 }
+
+
+// Uncomment to remove textwrapping on overflowing text in results panel headings
+/*
+for (var j = 0; j < headers.length; j++) {
+    headers[j].addEventListener('mouseover', function(e) {
+        e.preventDefault();
+        this.children[0].style.width = "100%";
+        this.children[1].style.display = "none";
+    });
+
+    headers[j].addEventListener('mouseout', function(e) {
+        e.preventDefault();
+        this.children[0].style.width = "80%";
+        this.children[1].style.display = "inline-block";
+    });
+}
+*/
+
+
+
 
