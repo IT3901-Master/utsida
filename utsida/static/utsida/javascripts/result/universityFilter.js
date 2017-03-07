ab326720f53783c08773c06f2a3d9b6626dd6938
Lars Liverød Andersen <lars_la@live.com>
2017-03-07 12:17:44 +0100

    + Made an alternative to the label filtering of universities on the reuslt page

---
+++ utsida/static/utsida/javascripts/result/universityFilter.js
@@ -0,0 +1,30 @@
+
+var s;
+
+UniversityFilter = {
+
+    settings: {
+        selector: $('#universitySelector'),
+        selected_university: null
+    },
+
+    init: function() {
+        s = this.settings;
+        this.updateSelector();
+    },
+
+    filter: function() {
+        s.selected_university = s.selector.find(":selected").val();
+        window.location = "/process/result/" + s.selected_university + '/';
+    },
+
+    updateSelector: function() {
+        var uni = decodeURI(window.location.href.split('/').slice(-2, -1)[0]);
+        s.selector.val(uni).prop('selected', true);
+        console.log("wh")
+    }
+
+};
+
+UniversityFilter.init();
+
